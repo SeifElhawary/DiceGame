f0846fd9872d296d156282e7992ea770cf5b51fc
Seif Haytham Kamal <seif.haitham.bis512500426@commerce.helwan.edu.eg>
2026-06-19 15:52:26 +0300

    Refactor dice game logic for two players

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-var dice = [
+var playerNumber1 = Math.floor(Math.random() * 6 );
+var images =[
     "./images/dice1.png",
     "./images/dice2.png",
     "./images/dice3.png",
     "./images/dice4.png",
     "./images/dice5.png",
-    "./images/dice6.png"
-];
-function play(playerIndex){
-    var random = Math.floor(Math.random()*dice.length);
-document.querySelectorAll(`img`)[playerIndex].setAttribute(`src`,dice[random]);
+    "./images/dice6.png",
+]
+
+var player1 =document.querySelectorAll(`img`)[0].setAttribute(`src`,images [playerNumber1]);
+var playerNumber2 =Math.floor(Math.random()*6 );
+var player2 = document.querySelectorAll(`img`)[1].setAttribute(`src`,images[playerNumber2]);
+
+
+if(playerNumber1 > playerNumber2){
+    document.querySelector(`h1`).innerHTML =`Player 1 Wins😍`;
 }
-play(0);
-play(1);
+else if(playerNumber2 > playerNumber1){
+        document.querySelector(`h1`).innerHTML =`Player 2 Wins😍`;
 
+}
+else if(playerNumber1 === playerNumber2){
+        document.querySelector(`h1`).innerHTML =`Draw😒`;
 
-
+}
 
 
